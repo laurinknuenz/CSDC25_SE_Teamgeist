2cd554525993dc371e262cb3b20dbdf43b9c9f5a
Far108 <92019144+Far108@users.noreply.github.com>
2024-01-08 12:16:30 +0100

    Register and Login, Playerinformation

--- src/server/server.js
+++ src/server/server.js
@@ -6,8 +6,13 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(process.cwd(), "/src/public/hello-world.html"));
+    res.sendFile(path.join(process.cwd(), "/src/public/index.html"));
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(process.cwd(), "/src/public/Player Information.html"));
+});
+
 
 app.listen(port, () => {
     console.log("Express: The server is now listening on http://localhost:3000/")
