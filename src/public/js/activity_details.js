12ad3056c52f918f90731a92a649ebb16de2428b
laurinknuenz <114426294+laurinknuenz@users.noreply.github.com>
2024-01-21 22:13:07 +0100

    Fixed delete team and added attending members list in activity

--- src/public/js/activity_details.js
+++ src/public/js/activity_details.js
@@ -29,6 +29,8 @@ function fetchActivityData() {
             activityType.innerHTML = activity.type;
             dateAndTime.innerHTML = new Date(activity.date).toLocaleDateString("de-DE", { weekday: "short", day: "2-digit", month: "2-digit", year: "numeric", hour: "2-digit", minute: "2-digit" });
             location.innerHTML = (activity.location);
+
+            setupAttendingMembers(activity.listOfAttendees);
         }).catch(error => console.error('Error fetching activity data:', error));
 }
 
@@ -66,4 +68,33 @@ function setupEventListeners() {
     document.getElementById("back_btn").onclick = () => {
         window.location.href = "/dashboard";
     }
+}
+
+function setupAttendingMembers(attendingMembers) {
+    const playerList = document.getElementById('playerList');
+    let allTeammembers = [];
+    fetch('/api/users/teammembers')
+        .then(response => response.json())
+        .then(response => {
+            allTeammembers.push(response.manager);
+            for (let player of response.teammembers) allTeammembers.push(player);
+        })
+        .then(() => {
+            console.log(attendingMembers);
+            console.log(allTeammembers);
+            for (let attendingMember of attendingMembers) {
+                for (let allMember of allTeammembers) {
+                    if (attendingMember._id.toString() == allMember.userId.toString()) {
+                        let name = allMember.firstname + " " + allMember.lastname;
+                        let attendance = attendingMember.attendance;
+
+                        const memberElement = document.createElement('div');
+                        memberElement.className = "player";
+                        memberElement.innerHTML = `<strong>${name} - ${attendance?"Nimmt teil.": "Hat abgesagt."}</strong>`;
+                        playerList.appendChild(memberElement);
+                    }
+                }
+            }
+        })
+        .catch(error => console.error('Error fetching team members:', error));
 }
