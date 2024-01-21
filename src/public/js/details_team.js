1a2115c8493346cfb123f4b77867a182436a9fc0
David <david.xing@stud.fh-campuswien.ac.at>
2024-01-21 16:47:26 +0100

    added burger menu, logout, dashboard current user

--- src/public/js/details_team.js
+++ src/public/js/details_team.js
@@ -3,6 +3,26 @@ document.addEventListener('DOMContentLoaded', () => {
     setupEventListeners();
 });
 
+document.getElementById('burgerMenuButton').addEventListener('click', function () {
+    var menuContent = document.getElementById('burgerMenuContent');
+    menuContent.style.display = menuContent.style.display === 'block' ? 'none' : 'block';
+});
+
+document.getElementById('logoutLink').addEventListener('click', function (event) {
+    event.preventDefault(); // Prevent default link behavior
+
+    fetch('/auth/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+    })
+        .then(response => {
+            if (response.ok) {
+                window.location.href = '/login'; // Redirect to the login page after logout
+            }
+        })
+        .catch(error => console.error('Error:', error));
+});
+
 function fetchTeamData() {
     // Assuming the team ID is stored in a session or a similar mechanism
     fetch('/api/teams/')
